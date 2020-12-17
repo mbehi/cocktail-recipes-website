664a15bf5f784b815c4594d1fe4813eb6de82200
mbehi <mohsin.behi@gmail.com>
2020-12-17 17:24:05 -0600

    created a image folder within the repo

---
+++ app.js
@@ -0,0 +1,24 @@
+// console.log("hello, world");
+
+var myVariableName;
+
+myVariableName = "Hello, World!";
+console.log(myVariableName); //exception output: Hello, World
+
+var promptQuestion = "What is the best starter pokemon?";
+var response = prompt(promptQuestion);
+console.log(response);
+
+var alertMessage;
+var color; //declare variable
+
+
+if (response === "Squirtle"){
+    alertMessage = "Excellent";
+    color = "";
+} else if (response === "")
+    {
+
+    }
+
+
