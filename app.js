216cf7dc02e092effc37004d1ac6a768ba95bbdb
mbehi <mohsin.behi@gmail.com>
2020-12-17 19:36:59 -0600

    added the age verification function in the app.js

--- app.js
+++ app.js
@@ -30,3 +30,30 @@ function Password() {
         var UserProvidedPassword = prompt("Are you Bad and Boozy?")
     } while(UserProvidedPassword !== Password);
 }
+Password();
+
+document.write("<br>");
+
+
+
+/* Function to validate your age */
+function myPrompt(newPrompt, accept, reject){
+    var response = prompt(newPrompt);
+    response = response.toLowerCase();
+    console.log(response);
+
+    var alertMessage;
+    if(response === "yes"){
+        alertMessage = accept;
+    } else {
+        while (response.toLowerCase() != "yes"){
+            alert(reject);
+            console.log(response);
+            response = prompt (newPrompt);
+        }
+        alertMessage = accept;
+        alert(alertMessage);
+    }
+    return alertMessage;
+}
+    myPrompt("Are you over the age of 21", "Congratulations!", "Sorry, Please wait until your 21 years old. SEE YOU LATER")
